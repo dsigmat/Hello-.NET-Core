3bdab50839fe2c161a609718b4bed229d301a601
Denis Sidorov <dsigmat@hotmail.com>
2019-10-30 14:20:11 +0600

    one

--- WelcomeDotNetCORE/Program.cs
+++ WelcomeDotNetCORE/Program.cs
@@ -7,7 +7,13 @@ namespace WelcomeDotNetCORE
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome, .NET Core!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"
+                После охоты в джунглях Хиппо-но-Хангуса,
+                Я привезу стаю диких Биппо - но - Бангусов!
+                Биппо - но - Бангусы, что из Хиппо - но - Хангуса, —
+                Лучше чем те, что живут в Диппо - но - Дангусе,
+                И умнее чем те, что вне Ниппо - но - Нангуса.
+                ");
         }
     }
 }
